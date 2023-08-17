6525b4b376ad7ebd9546c0057ffa45fcc3fe7eb6
Ben Harper <benjaminitonio@gmail.com>
2023-08-17 11:59:28 -0500

    Pieces drop now!

--- Scripts/tetromino.gd
+++ Scripts/tetromino.gd
@@ -8,6 +8,7 @@ var is_next_piece
 var pieces = []
 
 @onready var piece_scene = preload("res://Scenes/piece.tscn")
+@onready var timer = $Timer
 
 var tetromino_cells
 
@@ -24,3 +25,33 @@ func _ready():
 	if is_next_piece == false:
 		position = tetromino_data.spawn_position
 		wall_kicks = Shared.wall_kicks_i if tetromino_data.tetromino_type == Shared.Tetromino.BACON else Shared.wall_kicks_jlostz
+
+func _input(event):
+	if Input.is_action_just_pressed("left"):
+		move(Vector2.LEFT)
+	elif Input.is_action_just_pressed("right"):
+		move(Vector2.RIGHT)
+	elif Input.is_action_just_pressed("down"):
+		move(Vector2.DOWN)
+	elif Input.is_action_just_pressed("hard_drop"):
+		pass
+	elif Input.is_action_just_pressed("rotate_left"):
+		pass
+	elif Input.is_action_just_pressed("rotate_right"):
+		pass
+
+func move(direction: Vector2):
+	var new_position = calculate_global_position(direction, global_position)
+	if new_position:
+		global_position = new_position
+		return true
+	return false
+
+func calculate_global_position(direction: Vector2, starting_global_position: Vector2):
+	#TODO: chek for collision wit other tetrominos
+	#TODO: Check for collision with game bounds
+	return starting_global_position + direction * pieces[0].get_size().x
+
+
+func _on_timer_timeout():
+	move(Vector2.DOWN)
