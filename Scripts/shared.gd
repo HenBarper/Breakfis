ab52f784aad1d10b7552307232a0e5aaa3acc232
Ben Harper <benjaminitonio@gmail.com>
2023-08-16 21:47:04 -0500

    new border and grid that's stomach like

--- Scripts/shared.gd
+++ Scripts/shared.gd
@@ -35,3 +35,16 @@ var wall_kicks_jlostz = [
 	[Vector2(0,0), Vector2(-1,0), Vector2(-1,-1), Vector2(0, 2), Vector2(-1, 2)],
 	[Vector2(0,0), Vector2(1, 0), Vector2(1, 1), Vector2(0,-2), Vector2(1, -2)]
 ]
+
+var data = {
+	Tetromino.BACON: preload("res://Resources/bacon_piece_data.tres"),
+	Tetromino.EGG: preload("res://Resources/egg_piece_data.tres"),
+	Tetromino.PANCAKE: preload("res://Resources/pancake_piece_data.tres"),
+	Tetromino.SAUSAGE: preload("res://Resources/sausage_piece_data.tres"),
+	Tetromino.BANANA: preload("res://Resources/banana_piece_data.tres"),
+	Tetromino.WAFFLES: preload("res://Resources/waffles_piece_data.tres"),
+	Tetromino.WAFFLEZ: preload("res://Resources/wafflez_piece_data.tres")
+}
+
+var clockwise_rotation_matrix = [Vector2(0, -1), Vector2(1, 0)]
+var counter_clockwise_rotation_matrix = [Vector2(0,1), Vector2(-1, 0)]
